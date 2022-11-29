5ee3f05f5a2639dfe1b3cd8f657137b3c5dcaf4b
PolinaLeonovich <119355516+PolinaLeonovich@users.noreply.github.com>
2022-11-29 11:08:16 +0300

    Лабораторная работа 3

---
+++ Лабораторная работа 3/task_6.py
@@ -0,0 +1,13 @@
+list_numbers = [2, 90, -2, 8, -36, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
+
+max_number = list_numbers[0]
+for i, current_number in enumerate(list_numbers):
+    if current_number >= max_number:
+        max_number = current_number
+        index = i
+
+index_last = len(list_numbers) - 1
+
+list_numbers[index], list_numbers[index_last] = list_numbers[index_last], list_numbers[index]
+
+print(list_numbers)
